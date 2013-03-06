b481ee5fe404b8a4962527f892d94675b0df27bd
seece <pekka.vaananen@iki.fi>
2013-03-06 20:39:44 +0200

    add database url check

--- app.py
+++ app.py
@@ -6,6 +6,11 @@ port = 8080
 if 'PORT' in os.environ:
 	port = os.environ['PORT']
 
+if not 'DATABASE_URL' in os.environ:
+	raise Exception("No database URL!")
+else:
+		print("Database URL found.")
+
 class HelloWorld:
 	def index(self):
 		return "MORO :D!"
