089a97e45c42e7e924aee8864e9547f4422467b8
seece <pekka.vaananen@iki.fi>
2013-03-06 16:58:30 +0200

    add port env detection

--- app.py
+++ app.py
@@ -1,8 +1,15 @@
+import os
 import cherrypy
+
+port = 8080
+
+if PORT in os.environ:
+	port = os.environ['PORT']
 
 class HelloWorld:
 	def index(self):
 		return "MORO :D!"
 	index.exposed = True
 
+cherrypy.server.socket_port = port
 cherrypy.quickstart(HelloWorld())
